30ca7986544e2570e5f3de106c6a2ce88879a1d6
Sean <stsikteris1@unm.edu>
2024-11-07 18:35:28 -0700

    adding content...

--- _plugins/external-posts.rb
+++ _plugins/external-posts.rb
@@ -22,12 +22,12 @@ module ExternalPosts
       end
     end
 
-    # def fetch_from_rss(site, src)
-    #   xml = HTTParty.get(src['rss_url']).body
-    #   return if xml.nil?
-    #   feed = Feedjira.parse(xml)
-    #   process_entries(site, src, feed.entries)
-    # end
+    def fetch_from_rss(site, src)
+      xml = HTTParty.get(src['rss_url']).body
+      return if xml.nil?
+      feed = Feedjira.parse(xml)
+      process_entries(site, src, feed.entries)
+    end
 
     def process_entries(site, src, entries)
       entries.each do |e|
